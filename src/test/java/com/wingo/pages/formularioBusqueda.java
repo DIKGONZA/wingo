352d877f90cfcb88d64f676686c8e49548c4fedb
DIKSON <contacto@dikson.com.co>
2023-02-26 12:06:33 -0500

    se crea segundo escenario donde se busca vuelo internacional en usd y solo de ida

--- src/test/java/com/wingo/pages/formularioBusqueda.java
+++ src/test/java/com/wingo/pages/formularioBusqueda.java
@@ -4,14 +4,18 @@ import net.serenitybdd.screenplay.targets.Target;
 
 public class formularioBusqueda {
 
+    public static Target CHECK_SOLO_IDA = Target.the("check solo ida").locatedBy("//button[@id='btnIda']");
     public static Target INPUT_DESDE = Target.the("input origen").locatedBy("//input[@id='inputOrigen']");
     public static Target SELECT_DESDE = Target.the("input origen").locatedBy("//input[@id='inputOrigen']/preceding-sibling::div[@class=' styledSelect styledSelectOrigen']");
-    public static Target INPUT_HACIA = Target.the("input hacia").locatedBy("//input[@id='inputDestino']");
-    public static Target AEROPUERTO_ORIGEN = Target.the("Aeropuerto Origen").locatedBy("//li[@data-cod='MDE']");
-    public static Target AEROPUERTO_DESTINO = Target.the("Aeropuerto destino").locatedBy("//li[@data-cod='PUJ']");
+    public static Target AEROPUERTO_ORIGEN_MED = Target.the("Aeropuerto Origen de medellin").locatedBy("//li[@data-cod='MDE']");
+    public static Target AEROPUERTO_DESTINO_PUJ = Target.the("Aeropuerto destino punta cana").locatedBy("//li[@data-cod='PUJ']");
+    public static Target AEROPUERTO_DESTINO_CUN = Target.the("Aeropuerto destino cancun").locatedBy("//li[@data-cod='CUN']");
     public static Target INPUT_FECHA_IDA = Target.the("Input Fecha ida").locatedBy("//div[@id='btBalendarFrom']");
     public static Target FECHA_IDA = Target.the("Fecha Regreso").locatedBy("(//div[@id='inputOutboundDate']//descendant::table[@class='month1']//descendant::div[contains(@class,' valid')])[last()]");
     public static Target FECHA_REGRESO = Target.the("Fecha Regreso").locatedBy("(//div[@id='inputReturnDate']//descendant::table[@class='month2']//descendant::div[contains(@class,' valid')])[last()]");
+    public static Target FECHA_MES_2_ULTIMA_FECHA_DISPONIBLE = Target.the("ultima fecha disponble mes siguiente").locatedBy("(//div[@id='inputOutboundDate']//descendant::table[@class='month2']//descendant::div[contains(@class,' valid')])[last()]");
     public static Target BOTON_BUSCAR = Target.the("Botón Buscar").locatedBy("//div[@class='btn-search bt-search']");
+    public static Target INPUT_MONEDA = Target.the("Input moneda").locatedBy("//div[@class='styledSelect styledSelectMoneda']");
+    public static Target MONEDA_DOLAR = Target.the("MONEDA DOLAR").locatedBy("//ul[@id='currencies']/child::li[contains(text(),'USD')]");
 }
 
--- src/test/java/com/wingo/task/BuscarVueloInternacionalSoloIdaenUSD.java
+++ src/test/java/com/wingo/task/BuscarVueloInternacionalSoloIdaenUSD.java
@@ -7,19 +7,19 @@ import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.Enter;
 import net.serenitybdd.screenplay.actions.Open;
 
-public class BuscarVueloInternacional {
-    public static Performable simple(String origen,String destino) {
-        return Task.where("{0} busca una vuelo desde '" + origen + "' hacia '" + destino,
+public class BuscarVueloInternacionalSoloIdaenUSD {
+    public static Performable simple(String origen, String destino) {
+        return Task.where("{0} busca una vuelo desde '" + origen + "' hacia '" + destino + " solo de ida y en usd",
                 Open.url("https://www.wingo.com/es"),
+                Click.on(formularioBusqueda.CHECK_SOLO_IDA),
                 Click.on(formularioBusqueda.SELECT_DESDE),
                 Enter.theValue(origen).into(formularioBusqueda.INPUT_DESDE),
                 Click.on(formularioBusqueda.AEROPUERTO_ORIGEN_MED),
-                Click.on(formularioBusqueda.AEROPUERTO_DESTINO_PUJ),
-                Click.on(formularioBusqueda.INPUT_FECHA_IDA),
-                Click.on(formularioBusqueda.FECHA_IDA),
-                Click.on(formularioBusqueda.FECHA_REGRESO),
+                Click.on(formularioBusqueda.AEROPUERTO_DESTINO_CUN),
+                Click.on(formularioBusqueda.FECHA_MES_2_ULTIMA_FECHA_DISPONIBLE),
+                Click.on(formularioBusqueda.INPUT_MONEDA),
+                Click.on(formularioBusqueda.MONEDA_DOLAR),
                 Click.on(formularioBusqueda.BOTON_BUSCAR)
-                );
+        );
     }
-
 }
